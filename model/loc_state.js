db9e5298ebb2b89320a7cbcfd55441d5a3d378f8
aandrxx <59689393+aandrxx@users.noreply.github.com>
2022-10-27 17:03:46 +0300

    Add bookshelf model 'loc_state'

---
+++ model/loc_state.js
@@ -0,0 +1,16 @@
+const knex = require('../config/db').knex;
+const bookshelf = require('bookshelf')(knex);
+
+const LocState = bookshelf.Model.extend({
+    tableName: 'loc_state'
+}, {
+    find: function(filter, options) {
+        return this.forge().where(filter).fetchAll(options);
+    },
+
+    findOne: function(query, options) {
+        return this.forge(query).fetch(options);
+    },
+});
+
+module.exports = LocState;
