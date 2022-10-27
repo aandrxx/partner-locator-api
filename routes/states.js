717c5e86d43bd2b6bcff2dec0b611feb0053fe76
aandrxx <59689393+aandrxx@users.noreply.github.com>
2022-10-27 18:41:58 +0300

    Fix handle errors 400, 404, 500

--- routes/states.js
+++ routes/states.js
@@ -5,14 +5,29 @@ const LocState = require('../model/loc_state');
 router.get('/:id', async (req, res) => {
   if(req.params.id) {
     try {
-      res.status(200).json(await LocState.findOne({state_id: req.params.id}));
+      const state = await LocState.findOne({state_id: req.params.id}, { require: false });
+      if(state) {
+        res.status(200).json(state);
+      } else {
+        res.status(404).json({
+          errors: {
+              msg: 'NOT_FOUND'
+          }
+        });
+      }
     } catch (error) {
-      res.status(400).json({
+      res.status(500).json({
         errors: {
-            msg: 'BAD_REQUEST'
+            msg: 'SERVER_ERROR'
         }
-    });
+      });
     }
+  } else {
+    res.status(400).json({
+      errors: {
+          msg: 'BAD_REQUEST'
+      }
+    });
   }
 });
 
