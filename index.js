a3a7e28ea0ed1806c959752268a405d6f9147741
aandrxx <59689393+aandrxx@users.noreply.github.com>
2022-10-28 17:31:57 +0300

    Added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { initDb } = require('./config/db');
 const bodyParser = require('body-parser');
 
@@ -9,6 +10,7 @@ app.use(bodyParser.json({ limit: '20mb' }));
 // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
 
+app.use(cors());
 app.use(require('./routes'));
 
 app.listen(1337, () => console.log('Example app is listening on port 1337.'));
